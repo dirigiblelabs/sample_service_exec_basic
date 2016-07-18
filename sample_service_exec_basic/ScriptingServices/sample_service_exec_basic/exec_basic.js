45d5358473bc51cc91250c05d6980bef78d53e76
nedelcho-delchev-tues <nedelcho_delchev@abv.bg>
2016-07-18 12:54:21 -0400

    initial

---
+++ sample_service_exec_basic/ScriptingServices/sample_service_exec_basic/exec_basic.js
@@ -0,0 +1,17 @@
+/* globals $ */
+/* eslint-env node, dirigible */
+
+var exec = require('service/exec');
+var response = require('net/http/response');
+
+var context = {"input_param": "input_param_value"};
+
+// Test execution
+var result = exec.test("/service/exec_target_test.js", context);
+
+response.println("Printing output parameter: " + result.context.output_param);
+
+// use also exec.js(), exec.flow(), exec.job(), exec.sql(), exec.command() ...
+
+response.flush();
+response.close();
