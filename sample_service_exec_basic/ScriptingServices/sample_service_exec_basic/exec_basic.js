6d2498cb6f38186c10359fa5c2ce55ed697389bf
nedelcho-delchev-tues <nedelcho_delchev@abv.bg>
2016-07-23 09:00:12 -0400

    "Adding exec_target_basic.js so it executes properly"

--- sample_service_exec_basic/ScriptingServices/sample_service_exec_basic/exec_basic.js
+++ sample_service_exec_basic/ScriptingServices/sample_service_exec_basic/exec_basic.js
@@ -1,17 +1,17 @@
 /* globals $ */
-/* eslint-env node, dirigible */
+	/* eslint-env node, dirigible */
 
-var exec = require('service/exec');
-var response = require('net/http/response');
+	var exec = require('service/exec');
+	var response = require('net/http/response');
 
-var context = {"input_param": "input_param_value"};
+	var context = {"input_param": "input_param_value"};
 
-// Test execution
-var result = exec.test("/service/exec_target_test.js", context);
+	// Test execution
+	var result = exec.test("/service/exec_target_basic.js", context);
 
-response.println("Printing output parameter: " + result.context.output_param);
+	response.println("Printing output parameter: " + result.context.output_param);
 
-// use also exec.js(), exec.flow(), exec.job(), exec.sql(), exec.command() ...
+	// use also exec.js(), exec.flow(), exec.job(), exec.sql(), exec.command() ...
 
-response.flush();
-response.close();
+	response.flush();
+	response.close();
